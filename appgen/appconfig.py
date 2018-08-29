07198286782c8064425c8c40835ef63afc8bc2a8
Chris Erwin <crerwin@gmail.com>
2018-08-29 10:42:16 -0400

    custom dictionary for json dump in prep for properties to enforce types

--- appgen/appconfig.py
+++ appgen/appconfig.py
@@ -4,20 +4,27 @@ import yaml
 
 class AppConfig():
     def __init__(self):
-        self.id = ""
-        self.cmd = ""
-        self.cpus = 0.1
-        self.mem = 10.0
-        self.instances = 1
+        self._id = ""
+        self._cmd = ""
+        self._cpus = 0.1
+        self._mem = 10.0
+        self._instances = 1
 
     def loadyaml(self, input):
         y = yaml.load(input)
         print(y)
-        self.id = y['id']
-        self.cmd = y['cmd']
-        self.cpus = float(y['cpus'])
-        self.mem = float(y['mem'])
-        self.instances = int(y['instances'])
+        self._id = y['id']
+        self._cmd = y['cmd']
+        self._cpus = float(y['cpus'])
+        self._mem = float(y['mem'])
+        self._instances = int(y['instances'])
 
     def getjson(self):
-        return json.dumps(self.__dict__, indent=4)
+        d = {
+            "id": self._id,
+            "cmd": self._cmd,
+            "cpus": self._cpus,
+            "mem": self._mem,
+            "instances": self._instances
+        }
+        return json.dumps(d, indent=4)
