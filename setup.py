d66926b533326f775c261fcdf7b572975a44cb41
Chris Erwin <crerwin@gmail.com>
2018-09-10 17:07:05 -0400

    add tests for run()

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='appgen',
-    version='1.5.0',
+    version='1.5.1',
     packages=['appgen'],
     install_requires=['pyyaml'],
     entry_points={
--- test/test_appgen.py
+++ test/test_appgen.py
@@ -16,3 +16,25 @@ class AppgenTestCase(unittest.TestCase):
         self.assertEqual((False, ["file2.txt"]),
                          appgen.validfilenames("file1.json",
                                                "file2.txt"))
+
+    def test_run(self):
+        class Args():
+            def __init__(self, devconffile, opsconffile, defaultsfile,
+                         outputfile):
+                self.devconffile = devconffile
+                self.opsconffile = opsconffile
+                self.defaultsfile = defaultsfile
+                self.outputfile = outputfile
+
+        testargs = Args('test/testinput/testcase1/devconf.json',
+                        'test/testinput/testcase1/opsconf.json',
+                        'test/testinput/testcase1/defaults.json',
+                        None)
+        expect = open('test/testinput/testcase1/application.json').read()
+        self.assertEqual(expect, appgen.run(testargs))
+        testargs = Args('test/testinput/testcase2/devconf.json',
+                        'test/testinput/testcase2/opsconf.json',
+                        'test/testinput/testcase2/defaults.json',
+                        None)
+        expect = open('test/testinput/testcase2/application.json').read()
+        self.assertEqual(expect, appgen.run(testargs))
