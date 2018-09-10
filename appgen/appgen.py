96b453308e5b7073c04dc025d09e2588baab62e3
Chris Erwin <crerwin@gmail.com>
2018-09-10 11:32:06 -0400

    basic functionality

--- appgen/appgen.py
+++ appgen/appgen.py
@@ -1,8 +1,43 @@
 import sys
 import json
+from appgen.merger import Merger
+from appgen.parser import Parser
 
 
 def main():
+    app = {}
     args = sys.argv[1:]
-    dump = json.dumps([1, 2, 3, {'4': 5, '6': 7}], separators=(',', ':'))
-    print('args:', args)
+    parser = Parser()
+    merger = Merger()
+    if len(args) == 3:
+        devconffile = args[0]
+        opsconffile = args[1]
+        defaultsfile = args[2]
+        valid = validfilenames(devconffile, opsconffile, defaultsfile)
+        if valid[0]:
+            devconf = parser.loadfile(devconffile)
+            opsconf = parser.loadfile(opsconffile)
+            defaults = parser.loadfile(defaultsfile)
+            print("devconf: ", devconf)
+            print("opsconf: ", opsconf)
+            print("defaults: ", defaults)
+            app = merger.mergeconfigs(devconf, opsconf, defaults)
+            return app
+        else:
+            print("invalid file name(s): ", valid[1])
+    else:
+        print('appgen takes three arguments.  usage:'
+              + 'appgen devconf.[yml|yaml|json] opsconf.[yml|yaml|json]'
+              + ' defaults.[yml|yaml|json]')
+
+
+def validfilenames(*filenames):
+    valid = True
+    invalidnames = []
+    for filename in filenames:
+        if not (filename.endswith('json') or
+                filename.endswith('yaml') or
+                filename.endswith('yml')):
+                valid = False
+                invalidnames.append(filename)
+    return valid, invalidnames
