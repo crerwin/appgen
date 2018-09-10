6804f284d81f7beacd3e90886569e76e3afae76a
Chris Erwin <crerwin@gmail.com>
2018-09-10 13:41:06 -0400

    rename arguments for checkforconflicts

--- appgen/merger.py
+++ appgen/merger.py
@@ -2,16 +2,18 @@ class Merger():
     def __init__(self):
         self.app = {}
 
-    def checkforconflicts(self, devdict, opsdict):
+    def checkforconflicts(self, firstdict, seconddict):
+        # check for conflicting keys in two dictionaries
         conflict = False
         conflicting_key = None
-        for key in devdict:
-            if key in opsdict:
+        for key in firstdict:
+            if key in seconddict:
                 conflict = True
                 conflicting_key = key
         return conflict, conflicting_key
 
     def mergeconfigs(self, devdict, opsdict, defaults):
+        # return a mesosphere application configuration as a dict from 3 inputs
         app = {}
         if self.checkforconflicts(devdict, opsdict) == (False, None):
             if self.checkforconflicts(opsdict, defaults) == (False, None):
